3d02c0cc660fd93feb8d1948d6392e6d7fa7ca2a
Aravindh <94345681+AravindhKKS@users.noreply.github.com>
2025-01-05 12:36:02 +0530

    Update db.jsx

--- src/db.jsx
+++ src/db.jsx
@@ -6,16 +6,23 @@ export const ProjectItems = [
     Title: "E-Commerce",
     Desc: "Streamline your e-commerce experience with our React front-end. Enhanced usability, responsiveness, and performance for seamless online shopping.",
   },
-  {
+    {
     id: 2,
     Img: "assests/project2.png",
+    Link: "https://kks-real-estate.netlify.app/",
+    Title: "Real-Estate",
+    Desc: "A basic React-based real estate UI showcasing property listings with details like images, prices, and locations. Includes a search bar for filtering and a responsive design for easy browsing.",
+  },
+  {
+    id: 3,
+    Img: "assests/project3.png",
     Link: "https://kks-blog-post.netlify.app/",
     Title: "Blog-Post",
     Desc: "Engage readers with our captivating blog post. Compelling content and immersive storytelling for an unforgettable reading experience.",
   },
   {
-    id: 3,
-    Img: "assests/project3.png",
+    id: 4,
+    Img: "assests/project4.png",
     Link: "https://kks-social.netlify.app/",
     Title: "Socaial-Media",
     Desc: "Amplify your social media presence with our dynamic React front-end. Engaging interfaces and seamless interactions for an impactful social media experience.",
